39903434a44d3fc1d671c16592d30cd8bf00675d
atco21 <alfcom2@alu.edu.gva.es>
2024-10-16 13:18:34 +0200

    a1

--- sesion/auth.php
+++ sesion/auth.php
@@ -1,32 +1,40 @@
 <?php
 session_start(); // Iniciar sesión
 include('bd.php');
- 
+
 // Obtener los datos del formulario (POST)
 $usuario = $_POST['usuario'];
 $password = $_POST['password'];
 
 $encontrado = false;
 
-print_r($usuarios);
 
-$i=0;
+$i = 0;
 
 if (!empty($usuario) && !empty($password)) {
     
     while ($i < count($usuarios) && !$encontrado) {
         if ($usuarios[$i]['usuario'] == $usuario && $usuarios[$i]['password'] == $password) {
-            $encontrado = true; 
+            
+            
+            $_SESSION['usuario'] = $usuario;
+            $_SESSION['nombre'] = $usuarios[$i]['nombre'];
+            $_SESSION['rol'] = $usuarios[$i]['rol'];
+            
+            
+            header("Location: ./menu.php");
+            exit(); 
         }
         $i++;
     }
 
     if (!$encontrado) {
-    
-        $_SESSION['error'] = "usuario o contraseña incorrecta";
+        $_SESSION['error'] = "Usuario o contraseña incorrecta";
+        header("Location: ./login.php");
+        exit();
     }
 } else {
-
     $_SESSION['error'] = "Debe proporcionar un usuario y una contraseña";
+
 }
 ?>
